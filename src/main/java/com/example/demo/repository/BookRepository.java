f39f8ec8e3b9c0b42fe9ded320266dddf163cd8e
Oleksii Deineka <deineka219@gmail.com>
2024-11-23 20:10:46 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Book;
+
+import java.util.List;
+
+public interface BookRepository {
+    Book save(Book book);
+
+    List<Book> findAll();
+}
---
+++ src/main/java/com/example/demo/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,52 @@
+package com.example.demo.repository.impl;
+
+import java.util.List;
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    @Autowired
+    private final SessionFactory sessionFactory;
+
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save book into DB " + book);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(
+                    "SELECT u FROM Book u", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all books from DB ", e);
+        }
+    }
+}
