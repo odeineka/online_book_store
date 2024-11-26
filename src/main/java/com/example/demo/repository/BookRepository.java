9844af748b237af50c5ad844cedb0dd152d0ac1a
Oleksii Deineka <deineka219@gmail.com>
2024-11-26 17:34:41 +0200

    The part was done according to the requirements, since now on checkstyle.xml locates in config/checkstyle directory

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -2,9 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Book;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository {
-    Book save(Book book);
+
+    Book createBook(Book book);
 
     List<Book> findAll();
+
+    Optional<Book> findById(Long id);
 }
