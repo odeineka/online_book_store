4740a0027c91fb11a6bf5df8aca81abc42a66fee
Oleksii Deineka <deineka219@gmail.com>
2024-12-10 00:31:42 +0200

    added fixes after review

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,11 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Book;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
